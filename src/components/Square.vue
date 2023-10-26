8c687f9397faa7d2592851264d2839e3d8bed0d0
AndreSecco <andrefigueiro5@gmail.com>
2023-10-26 14:17:45 -0300

    Ajuste layout

--- src/components/Square.vue
+++ src/components/Square.vue
@@ -11,7 +11,7 @@ export default {
             this.$store.commit('makeMove', this.index);
         },
     },
-    computed: {
+    computed: { 
 
         // backgroundButton() {
         //     const currentPlayer = this.$store.state.currentPlayer;
